09be0bfe7acfd85b7f21c0f779e1ceb254a153b8
Görkem <grkkmm1@gmail.com>
2021-04-16 21:34:57 +0300

    add atom component generator

---
+++ generator/index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+const {
+  component,
+  exportComponent,
+  story,
+  style,
+  exportStyle,
+} = require('./templates/atom');
+
+const [name] = process.argv.slice(2);
+
+if (!name) throw new Error('You must include a component name.');
+
+const dir = `./src/components/atoms/${name}/`;
+const styleDir = `./src/components/atoms/${name}/styles`;
+
+if (fs.existsSync(dir))
+  throw new Error('A component with that name already exists.');
+
+fs.mkdirSync(dir);
+fs.mkdirSync(styleDir);
+
+function writeFileErrorHandler(err) {
+  if (err) throw err;
+}
+
+fs.writeFile(`${dir}/${name}.tsx`, component(name), writeFileErrorHandler);
+fs.writeFile(`${dir}/${name}.stories.tsx`, story(name), writeFileErrorHandler);
+fs.writeFile(`${dir}/index.ts`, exportComponent(name), writeFileErrorHandler);
+fs.writeFile(
+  `${dir}/styles/${name}.style.ts`,
+  style(name),
+  writeFileErrorHandler,
+);
+fs.writeFile(
+  `${dir}/styles/index.ts`,
+  exportStyle(name),
+  writeFileErrorHandler,
+);
---
+++ generator/templates/atom.js
@@ -0,0 +1,47 @@
+exports.component = (name) => {
+  return `import React from 'react';
+import { Styled${name} } from './styles';
+  
+export interface ${name}Props {
+  children: React.ReactNode;
+}
+  
+const ${name} = ({ children, ...rest }: ${name}Props): JSX.Element => (
+  <Styled${name} {...rest}>{children}</Styled${name}>
+);
+        
+export default ${name};\n`;
+};
+
+exports.story = (name) => {
+  return `import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import ${name}, { ${name}Props } from './${name}';
+      
+export default {
+  title: 'Atoms/${name}',
+  component: ${name},
+} as Meta;
+      
+const Template: Story<${name}Props> = (args) => <${name} {...args} />;
+      
+export const Default = Template.bind({});
+Default.args = {};\n`;
+};
+
+exports.exportComponent = (name) => {
+  return `export { default } from './${name}';
+export * from './${name}';\n`;
+};
+
+exports.style = (name) => {
+  return `import styled from 'styled-components';
+      
+type ${name}Props = {};
+  
+export const Styled${name} = styled.div<${name}Props>\`\`;\n`;
+};
+
+exports.exportStyle = (name) => {
+  return `export * from './${name}.style';\n`;
+};
