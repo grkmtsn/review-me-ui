079c5289f58f82a3bc6db6316594a7c3a297f740
Görkem <grkkmm1@gmail.com>
2021-06-24 15:20:55 +0300

    add bundle analyzer plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,30 @@
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import typescript from "rollup-plugin-typescript2";
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import typescript from 'rollup-plugin-typescript2';
+import analyze from 'rollup-plugin-analyzer';
 
-import packageJson from "./package.json";
+import packageJson from './package.json';
 
 export default {
-  input: "./src/index.ts",
+  input: './src/index.ts',
   output: [
     {
       file: packageJson.main,
-      format: "cjs",
+      format: 'cjs',
       sourcemap: true,
     },
     {
       file: packageJson.module,
-      format: "esm",
+      format: 'esm',
       sourcemap: true,
     },
   ],
-  plugins: [peerDepsExternal(), resolve(), commonjs(), typescript()],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript(),
+    analyze({ summaryOnly: true }),
+  ],
 };
